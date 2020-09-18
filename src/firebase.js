920f8abba37e61423b853b4c5f7850c535b3e677
mahankalpratik <mahankalpratik@gmail.com>
2020-09-18 12:48:34 +0530

    intital commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdMj0iq0hbbJ7Lkjv-BNuEewNFY-VZ6Iw",
+  authDomain: "todo-app-bf892.firebaseapp.com",
+  databaseURL: "https://todo-app-bf892.firebaseio.com",
+  projectId: "todo-app-bf892",
+  storageBucket: "todo-app-bf892.appspot.com",
+  messagingSenderId: "344380035354",
+  appId: "1:344380035354:web:e01a8b8c5efb9fb3eddca3",
+};
